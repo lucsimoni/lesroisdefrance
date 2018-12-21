a81c504f641ab89fc716ab5783e3c8ef57a90a22
lucsimoni <contact.lucsimoni@gmail.com>
2018-12-21 18:44:34 +0100

    Liens dans la navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './UI/top-bar/top-bar.component';
 import { FooterBarComponent } from './UI/footer-bar/footer-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TopBarComponent, FooterBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     TopBarComponent,
