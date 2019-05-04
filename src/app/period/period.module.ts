67d4faba60ad993d69bf0a2af104a2978960f707
lucsimoni <contact.lucsimoni@gmail.com>
2019-05-04 14:11:59 +0200

    Ajoute une page home pour periode copie de l'ancienne home

--- src/app/period/period.module.ts
+++ src/app/period/period.module.ts
@@ -5,9 +5,10 @@ import { PeriodRoutingModule } from './period-routing.module';
 import { PeriodPageComponent } from './components/period-page/period-page.component';
 import { CharacterCardComponent } from './components/period-page/components/character-card/character-card.component';
 import { PeriodHomeComponent } from './components/period-home/period-home.component';
+import { PeriodCardComponent } from './components/period-home/components/period-card/period-card.component';
 
 @NgModule({
-  declarations: [PeriodPageComponent, CharacterCardComponent, PeriodHomeComponent],
+  declarations: [PeriodPageComponent, CharacterCardComponent, PeriodHomeComponent, PeriodCardComponent],
   imports: [
     CommonModule,
     PeriodRoutingModule
