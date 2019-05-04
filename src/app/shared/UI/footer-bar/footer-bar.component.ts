62196df7e59301a715f4826f0aa4bb465209fff7
lucsimoni <contact.lucsimoni@gmail.com>
2019-05-04 10:29:49 +0200

    Barre de recherche

--- src/app/shared/UI/footer-bar/footer-bar.component.ts
+++ src/app/shared/UI/footer-bar/footer-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
