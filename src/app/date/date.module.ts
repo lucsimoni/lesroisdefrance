087b8df3714bf193587ecc8d386c42328c372be6
lucsimoni <contact.lucsimoni@gmail.com>
2019-05-11 14:18:54 +0200

    character card pour la date et footer fixed

--- src/app/date/date.module.ts
+++ src/app/date/date.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DateRoutingModule } from './date-routing.module';
 import { DatePageComponent } from './components/date-page/date-page.component';
 import { Ng5SliderModule } from 'ng5-slider';
+import { CharacterCardComponent } from './components/date-page/components/character-card/character-card.component';
 
 @NgModule({
-  declarations: [DatePageComponent],
+  declarations: [DatePageComponent, CharacterCardComponent],
   imports: [
     CommonModule,
     DateRoutingModule,
